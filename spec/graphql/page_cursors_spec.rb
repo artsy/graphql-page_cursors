6388eca8397ed320a30c688a9a41b15da9d53839
Jon Allured <jon.allured@gmail.com>
2020-11-10 14:35:52 -0600

    Update namespace to match main gem

--- spec/graphql/page_cursors_spec.rb
+++ spec/graphql/page_cursors_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-RSpec.describe Graphql::PageCursors do
+RSpec.describe GraphQL::PageCursors do
   it 'has a version number' do
-    expect(Graphql::PageCursors::VERSION).not_to be nil
+    expect(GraphQL::PageCursors::VERSION).not_to be nil
   end
 end
