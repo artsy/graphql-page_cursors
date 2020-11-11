43a6f59cf21bd4fd5884a9cf6d6208256c4beacc
Jon Allured <jon.allured@gmail.com>
2020-11-11 10:20:26 -0600

    Reorganize code

--- lib/graphql/page_cursors/version.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module GraphQL
-  module PageCursors
-    VERSION = '0.0.1'
-  end
-end
--- spec/graphql/page_cursors_spec.rb
+++ spec/graphql/page_cursors_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-RSpec.describe GraphQL::PageCursors do
+RSpec.describe PageCursors do
   it 'has a version number' do
-    expect(GraphQL::PageCursors::VERSION).not_to be nil
+    expect(PageCursors::VERSION).not_to be nil
   end
 end
